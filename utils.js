1fe4ffc780783e316be104035b076866296cf138
Kirlat <kirlat@yula.media>
2017-02-01 16:21:38 +0400

    Updated to determine number of column groups automatically

--- utils.js
+++ utils.js
@@ -1,10 +1,15 @@
 var blaze = blaze || {};
+blaze.utils = blaze.utils || {};
 
-// Equal height columns
-// Assign data-blz-eqht-col="i", where i is number from 1 to equalHeightColQty, to each column that should have equal height
-blaze.equalHeightColumns = function () {
-    var equalHeightColDAttr = "blaze-eqht-col",
-        equalHeightColQty = 10,
+/**
+ * Sets all columns with 'data-blaze-eqht-col' attribute to have equal height, if their group number is the same.
+ * Usage:
+ * add data-blaze-eqht="group_number" to elements that must have equal height.
+ * group_number is an integer starting from 1 and up to the maximum group number on a page
+ */
+blaze.utils.equalHeightColumns = function () {
+    var currentGroup,
+        groupQty = 0,
         i,
         j,
         currentHeight,
@@ -12,21 +17,25 @@ blaze.equalHeightColumns = function () {
         group;
 
     // Remove all values set previously
-    group = document.querySelectorAll('[data-blz-eqht-col]');
+    group = document.querySelectorAll('[data-blaze-eqht]');
     for (i = 0; i < group.length; i++) {
-        group[i].style.height = null;
+        // Find number of columns present
+        currentGroup = group[i].dataset.blazeEqht;
+        if (currentGroup > groupQty) {
+            groupQty = currentGroup;
+        }
     }
 
-    for (i = 1; i <= equalHeightColQty; i++) {
+    for (i = 1; i <= groupQty; i++) {
         currentHeight = 0;
         tallest = 0;
-        group = document.querySelectorAll("[data-"+equalHeightColDAttr+"='"+i+"']");
+        group = document.querySelectorAll("[data-blaze-eqht='" + i + "']");
         for (j = 0; j < group.length; j++) {
             currentHeight = group[j].clientHeight;
             if (currentHeight > tallest) tallest = currentHeight;
         }
         for (j = 0; j < group.length; j++) {
-            group[j].style.height = tallest + 'px';
+            group[j].style.minHeight = tallest + 'px';
         }
     }
 };
