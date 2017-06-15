9bded6247022eaef5e02bf113965b98d9977d112
Kirlat <kirlat@yula.media>
2017-06-15 19:31:44 +0400

    Added a function to parse parameters of a URL string

--- js/utils.js
+++ js/utils.js
@@ -39,3 +39,67 @@ blaze.utils.equalHeightColumns = function () {
         }
     }
 };
+
+/**
+ * Parses parameters of a URL string
+ * Based on the code from: https://www.sitepoint.com/get-url-parameters-with-javascript/
+ * @param url - a URL to parse
+ * @returns {{}} - An object in a format of parameter1: value1;
+ */
+blaze.utils.getURLParams = function (url) {
+    var params = {};
+
+    var queryString = url.split('?')[1];
+
+    // if URL is not empty
+    if (queryString) {
+
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
+
+        // split our query string into its component parts
+        var arr = queryString.split('&');
+
+        for (var i=0; i<arr.length; i++) {
+            // separate the keys and the values
+            var a = arr[i].split('=');
+
+            // in case params look like: list[]=thing1&list[]=thing2
+            var paramNum = undefined;
+            var paramName = a[0].replace(/\[\d*\]/, function(v) {
+                paramNum = v.slice(1,-1);
+                return '';
+            });
+
+            // set parameter value (use 'true' if empty)
+            var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+            // (optional) keep case consistent
+            paramName = paramName.toLowerCase();
+            paramValue = paramValue.toLowerCase();
+
+            // if parameter name already exists (for multiple parameters with the same name)
+            if (params[paramName]) {
+                // convert value to array (if still string)
+                if (typeof params[paramName] === 'string') {
+                    params[paramName] = [params[paramName]];
+                }
+                // if no array index number specified...
+                if (typeof paramNum === 'undefined') {
+                    // put the value on the end of the array
+                    params[paramName].push(paramValue);
+                }
+                // if array index number specified...
+                else {
+                    // put the value at that index number
+                    params[paramName][paramNum] = paramValue;
+                }
+            }
+            // if param name doesn't exist yet, set it
+            else {
+                params[paramName] = paramValue;
+            }
+        }
+    }
+    return params;
+};
